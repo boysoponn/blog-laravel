95b3303c84dcc9ebb092348a82d4273fc6adce9a
unknown <soponcmm@gmail.com>
2019-07-24 17:07:28 +0700

    gg

--- resources/views/component/cateList.blade.php
+++ resources/views/component/cateList.blade.php
@@ -3,27 +3,28 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        @if (isset($cate->category_id) && !empty($cate->category_id) && isset($cate->name) && !empty($cate->name))
                         <div class="col-4">
+                            @if (isset($cate->category_id) && !empty($cate->category_id) && isset($cate->name) && !empty($cate->name))
                             <a href="{{route('category',['id' => $cate->category_id])}}">{{Str::limit($cate->name, 100)}}</a>
+                            @endif
                         </div>
-                        @endif
-                        @if (isset($cate->description) && !empty($cate->description))
                         <div class="col-3">
-                            {{Str::limit($cate->description, 100)}}
+                            @if (isset($cate->description) && !empty($cate->description))
+                                {{Str::limit($cate->description, 100)}}
+                            @endif
                         </div>
-                        @endif
-                        @if (isset($cate->post) && !empty($cate->post) && isset($cate->comments) && !empty($cate->comments))
+                       
                         <div class="col-3">
-                            <small>จำนวนหัวข้อ {{$cate->post->count()}}</small>
-                            /
-                            <small>จำนวนกระทู้ {{$cate->comments->count() + $cate->post->count()}}</small>
+                            @if (isset($cate->post) && !empty($cate->post) && isset($cate->comments) && !empty($cate->comments))
+                                <small>จำนวนหัวข้อ {{$cate->post->count()}}</small>
+                                /
+                                <small>จำนวนกระทู้ {{$cate->comments->count() + $cate->post->count()}}</small>
+                            @endif
                         </div>
-                        @endif
                         <div class="col-2">
-                            
+
                             <small> 
-                                @if($cate->lastpost->created_at)
+                                @if (isset($cate->lastpost->created_at) && !empty($cate->lastpost->created_at))
                                     ล่าสุด {{$cate->lastpost->created_at->locale('th')->diffForHumans()}} 
                                 @endif
                                 @if(isset($cate->lastpost->user->user_id) && !empty($cate->lastpost->user->user_id) && isset($cate->lastpost->user->name) && !empty($cate->lastpost->user->name))
@@ -31,11 +32,10 @@
                                     โดย  {{$cate->lastpost->user->name}}
                                 </a>
                                 @endif
-                                @if($cate->lastpost->title && $cate->lastpost->title)
+                                @if (isset($cate->lastpost->title) && !empty($cate->lastpost->title))
                                     หัวข้อ {{$cate->lastpost->title}}
                                 @endif
                             </small>
-                            
                         </div>
                     </div>
                 </div>
